835b71f8c946a6e5e7e021ffd5d2846442e8b97e
huomengcheng <972642928@qq.com>
2020-06-09 10:55:47 +0800

    增加用户管理模块

---
+++ gmall0609-user-manage/src/main/java/com/hmc/gmall0609/user/Gmall0609UserManageApplication.java
@@ -0,0 +1,13 @@
+package com.hmc.gmall0609.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gmall0609UserManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gmall0609UserManageApplication.class, args);
+    }
+
+}
